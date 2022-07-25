b5d8c8f8cd7c060b388a9a9eea07b331c9ae2545
Claudio San Martin <claudio.sanmartinval@gmail.com>
2022-07-25 01:23:23 -0400

    Small style change

--- include/runAll_2DProj.C
+++ include/runAll_2DProj.C
@@ -1,13 +1,13 @@
 
 void runAll_2DProj()
 {
-    std::cout << "Data\n" << std::endl;
+    std::cout << " > Data" << std::endl;
     gROOT->ProcessLine(".x get2DProj.C(\"Fe\", true)");
     gROOT->ProcessLine(".x get2DProj.C(\"C\", true)");
     gROOT->ProcessLine(".x get2DProj.C(\"Pb\", true)");
     gROOT->ProcessLine(".x get2DProj.C(\"D\", true)");
 
-    std::cout << "Simulations\n" << std::endl;
+    std::cout << " > Simulations" << std::endl;
     gROOT->ProcessLine(".x get2DProj.C(\"Fe\", false)");
     gROOT->ProcessLine(".x get2DProj.C(\"C\", false)");
     gROOT->ProcessLine(".x get2DProj.C(\"Pb\", false)");
