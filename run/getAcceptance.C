21b19b6df76c1b83626740b3dee5cfe15a49fe39
Claudio San Martin <claudio.sanmartinval@gmail.com>
2022-08-22 23:10:37 -0400

    WIP Make Acceptance able to run in JLab Cluster

--- run/getAcceptance.C
+++ run/getAcceptance.C
@@ -5,14 +5,23 @@ R__LOAD_LIBRARY(../include/Acceptance_C.so)
 
 void getAcceptance(std::string target = "Fe", int binName = 0, int binNdim = 2, std::string nfold = "*")
 {
-  TChain ch("ntuple_sim");
+    TChain ch("ntuple_sim");
     if (FileExists("../../clas-HSim"))
     {
-        ch.Add(Form("../../clas-HSim/hsim_%s%s.root?#ntuple_sim",target.c_str(),nfold.c_str()));
+        ch.Add(Form("../../clas-HSim/hsim_%s%s.root",target.c_str(),nfold.c_str()));
     }
     else
     {
-        ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s%s/pruned%s_*.root",target.c_str(),nfold.c_str(),target.c_str())); // prunedFe_80.root
+        if (target=="D") { target+="2_pb"; }
+        if (nfold=="1" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s1/pruned%s_*.root",target.c_str(),target.c_str())); } // prunedFe_80.root
+        if (nfold=="2" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s2/pruned%s_*.root",target.c_str(),target.c_str())); }
+        if (nfold=="3" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s3/pruned%s_*.root",target.c_str(),target.c_str())); }
+        if (nfold=="4" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s4_yshiftm03/pruned%s_*.root",target.c_str(),target.c_str())); }
+        if (nfold=="5" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s5_yshiftm03/pruned%s_*.root",target.c_str(),target.c_str())); }
+        if (nfold=="6" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s6_yshiftm03/pruned%s_*.root",target.c_str(),target.c_str())); }
+        if (nfold=="7" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s7_yshiftm03/pruned%s_*.root",target.c_str(),target.c_str())); }
+        if (nfold=="8" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s8_yshiftm03/pruned%s_*.root",target.c_str(),target.c_str())); }
+        if (nfold=="9" || nfold=="*") { ch.Add(Form("../../sim/out/GetSimpleTuple_HSim/%s9_yshiftm03/pruned%s_*.root",target.c_str(),target.c_str())); }
     }
     // ch.Add("data/hsim_D2.root");
     // ch.Add("data/hsim_D3.root");
