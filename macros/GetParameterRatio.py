9edfee04e0af7d624ce8bdeee223916f5c769f9e
Claudio San Martin <claudio.sanmartinval@gmail.com>
2022-10-18 15:09:10 -0300

    Add Error in Parameters ratio

--- macros/GetParameterRatio.py
+++ macros/GetParameterRatio.py
@@ -15,6 +15,10 @@ myStyle.ForceStyle()
 gStyle.SetStatX(1 - myStyle.GetMargin() - 0.005)
 gStyle.SetStatY(2*myStyle.GetMargin() + 0.205)
 
+def PropErrorDivision(v1, e1, v2, e2):
+    this_error = TMath.Abs(v1/v2)*TMath.Sqrt((e1/v1)*(e1/v1) + (e2/v2)*(e2/v2)) # Assuming they both are uncorrelated
+    return this_error
+
 # Construct the argument parser
 parser = optparse.OptionParser("usage: %prog [options]\n")
 # parser.add_option('-x','--xlength', dest='xlength', default = 4.0, help="X axis range [-x, x]")
@@ -44,6 +48,7 @@ inputfile_D = TFile("%sFitFold_%s.root"%(inputPath_D,nameFormatted_D),"READ") if
 
 # list_solid_targets = ["C", "Fe", "Pb"]
 list_func_names = ["crossSection"] if fold else ["crossSectionL", "crossSectionR"]
+name_ext = "Fold" if len(list_func_names)==1 else "LR"
 
 infoDict = myStyle.getNameFormattedDict(dataset)
 
@@ -96,10 +101,18 @@ for i_h,h in enumerate(list_of_hists):
             par1_X = fit_solid.GetParameter(1)
             par2_X = fit_solid.GetParameter(2)
 
+            err0_X = fit_solid.GetParError(0)
+            err1_X = fit_solid.GetParError(1)
+            err2_X = fit_solid.GetParError(2)
+
             fit_D = hist_D.GetFunction(f)
             par0_D = fit_D.GetParameter(0)
             par1_D = fit_D.GetParameter(1)
             par2_D = fit_D.GetParameter(2)
+
+            err0_D = fit_D.GetParError(0)
+            err1_D = fit_D.GetParError(1)
+            err2_D = fit_D.GetParError(2)
 
             if verbose:
                 print("%s Fit: %s"%(hist_name, f))
@@ -111,32 +124,59 @@ for i_h,h in enumerate(list_of_hists):
                 print("       (%5.2f, %5.2f, %5.2f)"%( (par0_X/par0_X)/(par0_D/par0_D) , (par1_X/par0_X)/(par1_D/par0_D) , (par2_X/par0_X)/(par2_D/par0_D)))
                 print("")
 
-            ratio_th1_b_list[i_f].Fill(bin_name, (par1_X/par0_X)/(par1_D/par0_D))
-            ratio_th1_c_list[i_f].Fill(bin_name, (par2_X/par0_X)/(par2_D/par0_D))
-
             ratio_th1_b_pos_solid_list[i_f].Fill(bin_name, 0.0)
             ratio_th1_b_neg_solid_list[i_f].Fill(bin_name, 0.0)
             ratio_th1_c_pos_solid_list[i_f].Fill(bin_name, 0.0)
             ratio_th1_c_neg_solid_list[i_f].Fill(bin_name, 0.0)
 
-            if (par1_X/par0_X)>0: ratio_th1_b_pos_solid_list[i_f].SetBinContent(i_h+1, (par1_X/par0_X))
-            else: ratio_th1_b_neg_solid_list[i_f].SetBinContent(i_h+1, abs(par1_X/par0_X))
-            if (par2_X/par0_X)>0: ratio_th1_c_pos_solid_list[i_f].SetBinContent(i_h+1, (par2_X/par0_X))
-            else: ratio_th1_c_neg_solid_list[i_f].SetBinContent(i_h+1, abs(par2_X/par0_X))
+            err10_X = PropErrorDivision(par1_X, err1_X, par0_X, err0_X)
+            if (par1_X/par0_X)>0:
+                ratio_th1_b_pos_solid_list[i_f].SetBinContent(i_h+1, (par1_X/par0_X))
+                ratio_th1_b_pos_solid_list[i_f].SetBinError(i_h+1, err10_X)
+            else:
+                ratio_th1_b_neg_solid_list[i_f].SetBinContent(i_h+1, abs(par1_X/par0_X))
+                ratio_th1_b_neg_solid_list[i_f].SetBinError(i_h+1, err10_X)
+
+            err20_X = PropErrorDivision(par2_X, err2_X, par0_X, err0_X)
+            if (par2_X/par0_X)>0:
+                ratio_th1_c_pos_solid_list[i_f].SetBinContent(i_h+1, (par2_X/par0_X))
+                ratio_th1_c_pos_solid_list[i_f].SetBinError(i_h+1, err20_X)
+            else:
+                ratio_th1_c_neg_solid_list[i_f].SetBinContent(i_h+1, abs(par2_X/par0_X))
+                ratio_th1_c_neg_solid_list[i_f].SetBinError(i_h+1, err20_X)
+
+            ratio_th1_b_list[i_f].Fill(bin_name, 0.0)
+            ratio_th1_c_list[i_f].Fill(bin_name, 0.0)
+
+            err10_D = PropErrorDivision(par1_D, err1_D, par0_D, err0_D)
+            err1_XD = PropErrorDivision((par1_X/par0_X), err10_X, (par1_D/par0_D), err10_D)
+            ratio_th1_b_list[i_f].SetBinContent(i_h+1, (par1_X/par0_X)/(par1_D/par0_D))
+            ratio_th1_b_list[i_f].SetBinError(i_h+1, err1_XD)
+
+            err20_D = PropErrorDivision(par2_D, err2_D, par0_D, err0_D)
+            err2_XD = PropErrorDivision((par2_X/par0_X), err20_X, (par2_D/par0_D), err20_D)
+            ratio_th1_c_list[i_f].SetBinContent(i_h+1, (par2_X/par0_X)/(par2_D/par0_D))
+            ratio_th1_c_list[i_f].SetBinError(i_h+1, err2_XD)
 
 print("")
 
 canvas = TCanvas("cv","cv",1000,800)
 gStyle.SetOptStat(0)
+canvas.SetGrid(0,1)
 
 ### Ratio b/a and c/a per target
 outputPath = myStyle.getOutputDir("ParameterRatio",infoDict["Target"],rootpath)
 
-canvas.SetLogy(1)
+ymin = 0.001
+ymax = 1.2
+# canvas.SetLogy(0)
 for e,elem in enumerate(list_func_names):
-    sufix_name = "Fold"
-    if ("L" in elem): sufix_name = "L"
-    elif ("R" in elem): sufix_name = "R"
+    # sufix_name = "Fold"
+    # if ("L" in elem): sufix_name = "L"
+    # elif ("R" in elem): sufix_name = "R"
+
+    if ("L" in elem): name_ext = "L"
+    elif ("R" in elem): name_ext = "R"
 
     ## Ratio b/a
     legend_b = TLegend(1-myStyle.GetMargin()-0.35,1-myStyle.GetMargin()-0.12, 1-myStyle.GetMargin()-0.05,1-myStyle.GetMargin()-0.02)
@@ -147,14 +187,14 @@ for e,elem in enumerate(list_func_names):
     legend_b.SetFillStyle(0)
     ## Positive ratios
     hist_b_pos = ratio_th1_b_pos_solid_list[e]
-    hist_b_pos.SetMinimum(0.001)
-    hist_b_pos.SetMaximum(1.0)
+    hist_b_pos.SetMinimum(ymin)
+    hist_b_pos.SetMaximum(ymax)
 
     hist_b_pos.SetLineWidth(2)
     hist_b_pos.SetLineColor(ROOT.kBlue)
     legend_b.AddEntry(hist_b_pos,"Positive ratio")
 
-    hist_b_pos.Draw("hist")
+    hist_b_pos.Draw("hist e")
 
     hist_b_neg = ratio_th1_b_neg_solid_list[e]
 
@@ -162,13 +202,12 @@ for e,elem in enumerate(list_func_names):
     hist_b_neg.SetLineColor(ROOT.kRed)
     legend_b.AddEntry(hist_b_neg,"Negative ratio")
 
-    hist_b_neg.Draw("hist same")
+    hist_b_neg.Draw("hist e same")
 
     legend_b.Draw()
-    myStyle.DrawPreliminaryInfo("Parameters ratio %s"%sufix_name)
+    myStyle.DrawPreliminaryInfo("Parameters ratio %s"%name_ext)
     myStyle.DrawTargetInfo(nameFormatted, "Data")
-
-    canvas.SaveAs("%sRatio%s_b.gif"%(outputPath,sufix_name))
+    canvas.SaveAs("%s%s-Ratio%s_b.gif"%(outputPath,nameFormatted,name_ext))
     canvas.Clear()
 
     ## Ratio c/a
@@ -181,14 +220,14 @@ for e,elem in enumerate(list_func_names):
     legend_c.SetFillStyle(0)
     ## Positive ratios
     hist_c_pos = ratio_th1_c_pos_solid_list[e]
-    hist_c_pos.SetMinimum(0.001)
-    hist_c_pos.SetMaximum(1.0)
+    hist_c_pos.SetMinimum(ymin)
+    hist_c_pos.SetMaximum(ymax)
 
     hist_c_pos.SetLineWidth(2)
     hist_c_pos.SetLineColor(ROOT.kBlue)
     legend_c.AddEntry(hist_c_pos,"Positive ratio")
 
-    hist_c_pos.Draw("hist")
+    hist_c_pos.Draw("hist e")
 
     hist_c_neg = ratio_th1_c_neg_solid_list[e]
 
@@ -196,45 +235,48 @@ for e,elem in enumerate(list_func_names):
     hist_c_neg.SetLineColor(ROOT.kRed)
     legend_c.AddEntry(hist_c_neg,"Negative ratio")
 
-    hist_c_neg.Draw("hist same")
+    hist_c_neg.Draw("hist e same")
 
     legend_c.Draw()
-    myStyle.DrawPreliminaryInfo("Parameters ratio %s"%sufix_name)
+    myStyle.DrawPreliminaryInfo("Parameters ratio %s"%name_ext)
     myStyle.DrawTargetInfo(nameFormatted, "Data")
 
-    canvas.SaveAs("%sRatio%s_c.gif"%(outputPath,sufix_name))
+    canvas.SaveAs("%s%s-Ratio%s_c.gif"%(outputPath,nameFormatted,name_ext))
     canvas.Clear()
 
 ### Ratio of the ratios b/a and c/a -> Solid target / D target
 if "D" not in dataset:
-    canvas.SetLogy(0)
+    # canvas.SetLogy(0)
     for e,elem in enumerate(list_func_names):
-        sufix_name = "Fold"
-        if ("L" in elem): sufix_name = "L"
-        elif ("R" in elem): sufix_name = "R"
+        # sufix_name = "Fold"
+        # if ("L" in elem): sufix_name = "L"
+        # elif ("R" in elem): sufix_name = "R"
+
+        if ("L" in elem): name_ext = "L"
+        elif ("R" in elem): name_ext = "R"
 
         hist_b = ratio_th1_b_list[e]
-        hist_b.SetMinimum(-1.0)
-        hist_b.SetMaximum(1.0)
+        hist_b.SetMinimum(ymin)
+        hist_b.SetMaximum(2*ymax)
 
-        hist_b.Draw("hist")
+        hist_b.Draw("hist e")
 
-        myStyle.DrawPreliminaryInfo("Parameters ratio %s"%sufix_name)
+        myStyle.DrawPreliminaryInfo("Parameters ratio %s"%name_ext)
         myStyle.DrawTargetInfo(nameFormatted, "Data")
 
-        canvas.SaveAs("%sRatioOverD%s_b.gif"%(outputPath,sufix_name))
+        canvas.SaveAs("%s%s-RatioOverD_%s_b.gif"%(outputPath,nameFormatted,name_ext))
         canvas.Clear()
 
         hist_c = ratio_th1_c_list[e]
-        hist_c.SetMinimum(-1.0)
-        hist_c.SetMaximum(1.0)
+        hist_c.SetMinimum(ymin)
+        hist_c.SetMaximum(2*ymax)
 
-        hist_c.Draw("hist")
+        hist_c.Draw("hist e")
 
-        myStyle.DrawPreliminaryInfo("Parameters ratio %s"%sufix_name)
+        myStyle.DrawPreliminaryInfo("Parameters ratio %s"%name_ext)
         myStyle.DrawTargetInfo(nameFormatted, "Data")
 
-        canvas.SaveAs("%sRatioOverD_%s_c.gif"%(outputPath,sufix_name))
+        canvas.SaveAs("%s%s-RatioOverD_%s_c.gif"%(outputPath,nameFormatted,name_ext))
         canvas.Clear()
 
 print("Made it to the end!")
