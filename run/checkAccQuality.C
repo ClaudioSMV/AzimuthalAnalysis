8d49ec7e34216d5e625827d03f31afd979fd2046
ClaudioSMV <claudio.sanmartinval@gmail.com>
2022-12-22 11:13:56 -0300

    Add Error hist in quality of Acc and cut extension support

--- run/checkAccQuality.C
+++ run/checkAccQuality.C
@@ -8,25 +8,37 @@
 
 using namespace DIS;
 
-void checkAccQuality(std::string target = "Fe", int binName = 0)
+void checkAccQuality(std::string target = "Fe", int binName = 0, std::string acc_cuts = "")
 {
     std::vector<std::vector<double>> this_binning = DIS::Bin_List[binName];
 
-    TFile *facc = TFile::Open(Form("../output/JLab_cluster/Acceptance/Acceptance_%s_B%i.root", target.c_str(), binName), "READ");
-    TFile *fout = TFile::Open(Form("Quality_%s_B%i.root", target.c_str(), binName), "RECREATE");
+    if (acc_cuts!="")
+    {
+        acc_cuts = "_" + acc_cuts;
+    }
+    std::string acc_path = Form("../output/JLab_cluster/Acceptance%s",acc_cuts.c_str());
+    std::string out_path = Form("../output/JLab_cluster/QualityCheck%s",acc_cuts.c_str());
+    CreateDir(out_path);
+
+    TFile *facc = TFile::Open(Form("%s/Acceptance_%s_%iB.root", acc_path.c_str(), target.c_str(), binName), "READ");
+    TFile *fout = TFile::Open(Form("%s/Quality_%s_%iB.root", out_path.c_str(), target.c_str(), binName), "RECREATE");
 
     // Get Acceptance THnSparse
     THnSparse *histAcc_Reconstru = (THnSparse*)facc->Get("histAcc_Reconstru");
     THnSparse *histAcc_ReMtch_mc = (THnSparse*)facc->Get("histAcc_ReMtch_mc");
     THnSparse *histAcc_ReMtch_re = (THnSparse*)facc->Get("histAcc_ReMtch_re");
 
-    TH1D *hPQEmpty_Reconstru = new TH1D("hPQEmpty_Reconstru", "hPQEmpty_Reconstru", this_binning[4].size(), -180., 180.);
-    TH1D *hPQEmpty_ReMtch_mc = new TH1D("hPQEmpty_ReMtch_mc", "hPQEmpty_ReMtch_mc", this_binning[4].size(), -180., 180.);
-    TH1D *hPQEmpty_ReMtch_re = new TH1D("hPQEmpty_ReMtch_re", "hPQEmpty_ReMtch_re", this_binning[4].size(), -180., 180.);
+    TH1D *hPQEmpty_Reconstru = new TH1D("hPQEmpty_Reconstru", "hPQEmpty_Reconstru", this_binning[4].size()-1, -180., 180.);
+    TH1D *hPQEmpty_ReMtch_mc = new TH1D("hPQEmpty_ReMtch_mc", "hPQEmpty_ReMtch_mc", this_binning[4].size()-1, -180., 180.);
+    TH1D *hPQEmpty_ReMtch_re = new TH1D("hPQEmpty_ReMtch_re", "hPQEmpty_ReMtch_re", this_binning[4].size()-1, -180., 180.);
 
     TH1D *hAccVal_Reconstru = new TH1D("hAccVal_Reconstru", "hAccVal_Reconstru", 110, 0.0, 1.1);
     TH1D *hAccVal_ReMtch_mc = new TH1D("hAccVal_ReMtch_mc", "hAccVal_ReMtch_mc", 110, 0.0, 1.1);
     TH1D *hAccVal_ReMtch_re = new TH1D("hAccVal_ReMtch_re", "hAccVal_ReMtch_re", 110, 0.0, 1.1);
+
+    TH1D *hAccErr_Reconstru = new TH1D("hAccErr_Reconstru", "hAccErr_Reconstru", 300, 0.0, 0.3);
+    TH1D *hAccErr_ReMtch_mc = new TH1D("hAccErr_ReMtch_mc", "hAccErr_ReMtch_mc", 300, 0.0, 0.3);
+    TH1D *hAccErr_ReMtch_re = new TH1D("hAccErr_ReMtch_re", "hAccErr_ReMtch_re", 300, 0.0, 0.3);
 
     for (int Qi=0; Qi<this_binning[0].size()-1; Qi++)
     {
@@ -53,6 +65,10 @@ void checkAccQuality(std::string target = "Fe", int binName = 0)
                             std::cout << Form("R1: Bin %i is empty! >> (%.2f, %.2f, %.2f, %.2f, %.2f)",bin_Reconstru,kinVars[0],kinVars[1],kinVars[2],kinVars[3],kinVars[4]) << std::endl;
                             hPQEmpty_Reconstru->Fill(kinVars[4]);
                         }
+                        else
+                        {
+                            hAccErr_Reconstru->Fill(histAcc_Reconstru->GetBinError(bin_Reconstru));
+                        }
 
                         hAccVal_ReMtch_mc->Fill(val_ReMtch_mc);
                         if (val_ReMtch_mc==0)
@@ -60,12 +76,20 @@ void checkAccQuality(std::string target = "Fe", int binName = 0)
                             std::cout << Form("R2: Bin %i is empty! >> (%.2f, %.2f, %.2f, %.2f, %.2f)",bin_ReMtch_mc,kinVars[0],kinVars[1],kinVars[2],kinVars[3],kinVars[4]) << std::endl;
                             hPQEmpty_ReMtch_mc->Fill(kinVars[4]);
                         }
+                        else
+                        {
+                            hAccErr_ReMtch_mc->Fill(histAcc_ReMtch_mc->GetBinError(bin_ReMtch_mc));
+                        }
 
                         hAccVal_ReMtch_re->Fill(val_ReMtch_re);
                         if (val_ReMtch_re==0)
                         {
                             std::cout << Form("R3: Bin %i is empty! >> (%.2f, %.2f, %.2f, %.2f, %.2f)",bin_ReMtch_re,kinVars[0],kinVars[1],kinVars[2],kinVars[3],kinVars[4]) << std::endl;
                             hPQEmpty_ReMtch_re->Fill(kinVars[4]);
+                        }
+                        else
+                        {
+                            hAccErr_ReMtch_re->Fill(histAcc_ReMtch_re->GetBinError(bin_ReMtch_re));
                         }
                     }
                 }
