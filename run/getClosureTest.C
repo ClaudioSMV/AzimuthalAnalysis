d8447d62f0413a6818617055095510802bcaf9ab
ClaudioSMV <claudio.sanmartinval@gmail.com>
2023-02-18 20:28:54 -0300

    Change order in TChain of files to avoid possible biasing

--- run/getClosureTest.C
+++ run/getClosureTest.C
@@ -17,14 +17,14 @@ void getClosureTest(std::string target = "Fe", int binName = 0, int binNdim = 1,
         std::string suffix = "";
         if (target=="D") { suffix+="2_pb"; }
         if (nfold=="1" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s1/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); } // prunedFe_80.root
-        if (nfold=="2" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s2/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
-        if (nfold=="3" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s3/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
-        if (nfold=="4" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s4_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
-        if (nfold=="5" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s5_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
-        if (nfold=="6" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s6_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
-        if (nfold=="7" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s7_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
-        if (nfold=="8" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s8_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
         if (nfold=="9" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s9_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
+        if (nfold=="2" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s2/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
+        if (nfold=="8" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s8_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
+        if (nfold=="3" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s3/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
+        if (nfold=="7" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s7_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
+        if (nfold=="4" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s4_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
+        if (nfold=="6" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s6_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
+        if (nfold=="5" || nfold=="*") { ch.Add(Form("/home/csanmart/work/sim/out/GetSimpleTuple_HSim/%s%s5_yshiftm03/pruned%s_*.root",target.c_str(),suffix.c_str(),target.c_str())); }
     }
     
     Acceptance acc(&ch);
