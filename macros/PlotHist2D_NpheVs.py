fa9292a5ec5af36c14af54572ed7ff1202557d46
ClaudioSMV <claudio.sanmartinval@gmail.com>
2023-04-20 16:55:48 -0400

    Add Plot NpheVs (aka line of death)

---
+++ macros/PlotHist2D_NpheVs.py
@@ -0,0 +1,81 @@
+from ROOT import TFile,TTree,TCanvas,TH1D,TH1F,TH2D,TH2F,THStack,TLatex,TMath,TColor,TLegend,TEfficiency,TLine,gROOT,gPad,TF1,gStyle,kBlack,kWhite,TH1
+import ROOT
+import os
+import optparse
+import myStyle as ms
+import math
+
+gROOT.SetBatch( True )
+gStyle.SetOptFit(1011)
+
+## Defining Style
+ms.ForceStyle(True)
+# gStyle.SetPadRightMargin(2*ms.GetMargin())
+# gStyle.SetPadTopMargin(1.1*ms.GetMargin())
+# gStyle.SetLabelSize(ms.GetSize()-10,"z")
+# gStyle.SetTitleYOffset(1.3)
+# gROOT.ForceStyle()
+
+# Construct the argument parser
+parser = optparse.OptionParser("usage: %prog [options]\n")
+parser.add_option('-D', dest='Dataset', default = "", help="Dataset in format <targ>")
+parser.add_option('-p', dest='rootpath', default = "", help="Add path to files, if needed")
+# parser.add_option('-l', dest='drawLines', action='store_true', default = False, help="Draw lines to see bins")
+parser.add_option('-d', dest='isData', action='store_true', default = False, help="HSim: False (default); Data: True")
+parser.add_option('-J', dest='JLabCluster', action='store_true', default = False, help="Use folder from JLab_cluster")
+
+parser.add_option('-i', dest='inputCuts', default = "", help="Add input cuts Xf_Yb_...")
+
+# IDEA: input format->  <target>_<binningType number>_<non-integrated dimensions> ; ex: Fe_0_2
+options, args = parser.parse_args()
+
+dataset = options.Dataset
+rootpath = options.rootpath
+isJLab = options.JLabCluster
+
+# drawLines = options.drawLines
+isData = options.isData
+this_targ = dataset
+
+## Cuts
+input_cuts = options.inputCuts
+
+## Input
+inputPath = ms.getOutputFolder("Hist2D", input_cuts, isJLab, False) # "../output/"
+inputPath += "NpheVs_"+this_targ+"_"
+inputPath += "data.root" if isData else "hsim.root"
+inputfile = TFile(inputPath,"READ")
+
+## Output
+outputPath = ms.getPlotsFolder("Hist2D/NpheVs", input_cuts, "", isJLab)
+
+canvas = TCanvas("cv","cv",1000,800)
+gStyle.SetOptStat(0)
+
+for i_h,h in enumerate(inputfile.GetListOfKeys()):
+    if ((h.ReadObj().Class_Name() == "TH2D")): #  and ("Corr_Reconstru" in h.GetName())
+        # hist_target = h.ReadObj()
+        hist_name = h.GetName() # hist2D_Nphe_vsThetaLabEl
+        hist2D = inputfile.Get(hist_name)
+
+        list_name = hist_name.split("_vs") # hist2D_Nphe , ThetaLabEl
+        nameXvar = list_name[1]
+        nameYvar = list_name[0].split("_")[1] # Nphe
+
+        hist2D.GetZaxis().SetMaxDigits(3)
+        hist2D.Draw("colz")
+
+        # ms.DrawPreliminaryInfo("P_{#pi^{+}} vs N_{phe} map")
+        ms.DrawPreliminaryInfo(hist2D.GetTitle())
+        dataOrSim = "Data" if isData else "Simulation"
+        out_DatOrSim = "Data" if isData else "HSim"
+
+        ms.DrawTargetInfo(this_targ, dataOrSim)
+
+        name_png = ms.getPlotsFile("%sVS%s_%s"%(nameYvar,nameXvar, this_targ),"","png",out_DatOrSim)
+        canvas.SaveAs(outputPath+name_png)
+
+        # name_pdf = ms.getPlotsFile("PvsNphe_%s"%(this_targ),"","pdf",out_DatOrSim)
+        # canvas.SaveAs(outputPath+name_pdf)
+
+        canvas.Clear()
