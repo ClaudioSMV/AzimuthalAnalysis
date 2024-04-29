3d62aaf22ad728077143c081e98db15fb5465253
ClaudioSMV <claudio.sanmartinval@gmail.com>
2024-04-29 14:56:18 -0400

    Redefine functions and update Fit macro to support Vs_X format

--- macros/Plot_Fit.py
+++ macros/Plot_Fit.py
@@ -10,209 +10,133 @@ from array import array
 
 ## Defining Style
 ms.force_style()
+gStyle.SetTitleYOffset(1.2)
+# Show Fit stats in correct position
 gStyle.SetOptFit(1)
+margin = ms.get_margin()
+gStyle.SetStatX(1 - margin - 0.005)
+gStyle.SetStatY(2*margin + 0.205)
 
-gStyle.SetStatX(1 - ms.get_margin() - 0.005)
-gStyle.SetStatY(2*ms.get_margin() + 0.205)
-
+colors = ms.get_color()
 
 ## Define functions
 
-def get_corrected_hist(h_input, h_name, this_fittype):
-# Return a copy of the corrected histogram
-    # Define initial and final bins depending on fit method
-    Nbins_in = h_input.GetXaxis().GetNbins()
-    bin_frst = 0
-    bin_last = Nbins_in
-    l_xbin_out = []
-    # if (this_fittype == "LR"):
-    if (this_fittype == "Fd"):
-        bin0 = h_input.FindBin(0.0)
-        bin_frst = bin0
-
-        l_xbin_out.append(0.0)
-    # elif (this_fittype == "Sh"):
-    # elif (this_fittype == "Ff"):
-
-    # Fill list with x-axis bin's limits
-    for bt in range(bin_frst, bin_last+1):
-        l_xbin_out.append(h_input.GetBinLowEdge(bt+1))
-
-    # Create output histogram
-    phi_axis_title = ms.axis_label('I',"LatexUnit") # "#phi_{PQ} (deg)"
-    h_out_title = ";%s;Counts"%(phi_axis_title)
-    Nbins_out = len(l_xbin_out)-1
-
-    h_output = TH1D(h_name, h_out_title, Nbins_out, array('d',l_xbin_out))
-
-    # Fill output histogram
-    for b in range(1, Nbins_in+1):
-        # Retrieve values from original hist
-        value = h_input.GetBinContent(b)
-        error = h_input.GetBinError(b)
-        this_bin = b
-
-        # if (this_fittype == "LR"):
-        if (this_fittype == "Fd"):
-
-            x_center_in = h_input.GetBinCenter(b)
-
-            bin_out = h_output.FindBin(abs(x_center_in))
-            value_out = h_output.GetBinContent(bin_out)
-            error_out = h_output.GetBinError(bin_out)
-
-            # If bin is not empty, then you're running over the second tail
-            # and must add the new content
-            if (value_out > 1.0):
-            # if (value_out != 0.0):
-                value+=value_out
-                error = TMath.Sqrt(error_out*error_out + error*error)
-
-            this_bin = bin_out
-        # elif (this_fittype == "Sh"):
-        # elif (this_fittype == "Ff"):
-
-        # Debug
-        # print(this_bin)
-
-        h_output.SetBinContent(this_bin, value)
-        h_output.SetBinError(this_bin, error)
-
-    return h_output
-
-def get_fit_functions(h_out, l_fname, fit_meth, opts):
-# Return list of TF1 with the proper range
-    ### Options:
-    ###     Skip0: Skip central peak
-    ###     Sin: Use sin(x) term in fit
-
-    # if (fit_meth == "LR"):
-    # elif (fit_meth == "Fd"):
-    # elif (fit_meth == "Sh"):
-    # elif (fit_meth == "Ff"):
-
-    # Define options: ["Fs", "NP",]
-    opt_sk0 = ms.cut_is_included("NP", opts)
-    opt_sin = ms.cut_is_included("Fs", opts)
-
-    # Set limits
-    Nbins = h_out.GetXaxis().GetNbins()
-
-    ## Option: Skip peak's bin (Problematic)
-    peak_Lend, peak_Rend = 0.0, 0.0
-    if (opt_sk0):
-        # Define peak bin as the one containing zero when using odd nbins
-        # and as both pre-and-post bins to zero with even nbins
-        bin_peak = h_out.FindBin(0.0)
-        d_peak_end_odd = {
-            "LR": [bin_peak, bin_peak+1], "Fd": [0, 2],
-            "Sh": [Nbins+1, 2], "Ff": [0, 0],
-        }
-        d_peak_end_even = {
-            "LR": [bin_peak-1, bin_peak+1], "Fd": [0, 2],
-            "Sh": [Nbins, 2], "Ff": [0, 0],
-        }
-
-        bin_odd = ((Nbins%2) == 1)
-
-        # Define limits of the peak for the fit function
-        if bin_odd:
-            peak_Lend = h_out.GetBinLowEdge(d_peak_end_odd[fit_meth][0])
-            peak_Rend = h_out.GetBinLowEdge(d_peak_end_odd[fit_meth][1])
-        else:
-            peak_Lend = h_out.GetBinLowEdge(d_peak_end_even[fit_meth][0])
-            peak_Rend = h_out.GetBinLowEdge(d_peak_end_even[fit_meth][1])
-
-    xmin_out = h_out.GetBinLowEdge(1)
-    xmax_out = h_out.GetBinLowEdge(Nbins+1)
-    list_limits = []
-    # Define limits of the fit functions
-    ## for [Right, Left*]
-    for i,func in enumerate(l_fname):
-        this_xmin, this_xmax = xmin_out, xmax_out
-
-        if (fit_meth == "LR"):
-            this_xmin = peak_Rend if (i==0) else  xmin_out
-            this_xmax =  xmax_out if (i==0) else peak_Lend
-        elif (fit_meth == "Fd"):
-            this_xmin = peak_Rend
-            this_xmax =  xmax_out
-        elif (fit_meth == "Sh"):
-            this_xmin = peak_Rend if (peak_Rend != 0.0) else xmin_out
-            this_xmax = peak_Lend if (peak_Lend != 0.0) else xmax_out
-        # elif (fit_meth == "Ff"):
-
-        these_limits = [this_xmin, this_xmax]
-        list_limits.append(these_limits)
-
-    # Define function
-    the_func = "[0] + [1]*cos(TMath::Pi()*x/180.0) +"\
-               " [2]*cos(2*TMath::Pi()*x/180.0)"
-    if opt_sin:
-        the_func+= "+ [3]*sin(TMath::Pi()*x/180.0)"
-
-    tf1_fit = []
-    # Create TF1 and return
-    ## for [Right, Left*]
-    for i,fname in enumerate(l_fname):
-        this_xmin = list_limits[i][0]
-        this_xmax = list_limits[i][1]
-
-        this_func = TF1(fname,the_func, this_xmin,this_xmax)
-        tf1_fit.append(this_func)
-
-    return tf1_fit
-
-
-def get_chi2ndf(f_fit, x,y, meth_ffit):
-    chisq = f_fit.GetChisquare()
-    ndf = f_fit.GetNDF()
-
-    fit_str = "#chi^{2} / ndf = %.2f / %i"%(chisq, ndf)
-    if (ndf != 0):
-        fit_str+= " = %.2f"%(chisq/ndf)
-    fit_str = TLatex(x,y, fit_str)
-
-    # 23: use center as reference (default method)
-    text_orientation = 23
-    if ("LR" in meth_ffit):
-        # 13: use left corner as reference (for R method);
-        # 33: use right corner as reference (for L method)
-        text_orientation = 13 if ("0" in meth_ffit) else 33
-    # elif (this_fittype == "Fd"):
-    # elif (this_fittype == "Sh"):
-    # elif (this_fittype == "Ff"):
-
-    fit_str.SetTextAlign(text_orientation)
-    fit_str.SetTextSize(ms.get_size()-6)
-
-    return fit_str
-
-def check_fit_feasibility(hist, fit_method):
-# Check if histogram can receive a proper fit treatment
-    hname = hist.GetName()
-    hentries = hist.GetEntries()
-    Nbins = hist.GetXaxis().GetNbins()
-    problem = False
-    if (hentries == 0):
-        info_txt = "Histogram %s is empty! Fit is not possible."%hname
-        problem = True
-    elif (hentries < 4): # ndf cut
+def histogram_has_problems(histogram, fit_method):
+# Check if histogram has problems that can affect the fit performance
+    hist_name = histogram.GetName()
+    # Since Correction fills each bin with SetBinContent, GetEntries gives the
+    # number of bins with a non-zero value
+    hist_entries = histogram.GetEntries()
+    hist_nbins = histogram.GetXaxis().GetNbins()
+    has_problems = False
+    # Skip empty histograms!
+    if (hist_entries == 0):
+        info_txt = "Histogram %s is empty! Fit is not possible."%(hname)
+        has_problems = True
+    # Report histograms without enough bins to perform fit (4 ndf needed)
+    elif (hist_entries < 4):
         info_txt = "Histogram %s has not enough bins!"\
-                   " Fit is not feasible."%hname
-        problem = True
-    elif ((fit_method is "LR") and (hentries < Nbins/2.)):
+                   " Fit is not feasible."%(hname)
+        has_problems = True
+    elif ((fit_method is "LR") and (hist_entries < hist_nbins/2.)):
         info_txt = "Histogram %s has half or more empty bins!"\
                    " Fit LR is not feasible."%hname
-        problem = True
+        has_problems = True
 
-    is_good = True
-    if problem:
-        ms.info_msg("Fit",info_txt)
-        is_good = False
+    # Report if any problem has appeared
+    if has_problems:
+        ms.info_msg("Fit", info_txt)
 
-    return is_good
+    return has_problems
 
+def get_correct_histogram_to_fit(histogram, hname, fit_method):
+# Return copy of histogram with the adequate distribution according to the
+# fit method selected
+    nbins = histogram.GetXaxis().GetNbins()
+    bin_zero = histogram.FindBin(0.0)
+    # Get bin limits to create corrected histogram to fit
+    idx_i = 1 if (fit_method != "Fd") else bin_zero
+    idx_f = nbins + 1
+    list_limits = [histogram.GetBinLowEdge(b) for b in range(idx_i, idx_f + 1)]
+
+    htitle_x = histogram.GetXaxis().GetTitle()
+    htitle_y = histogram.GetYaxis().GetTitle()
+    htitle = ";%s;%s"%(htitle_x, htitle_y)
+    nbins_to_fit = len(list_limits) - 1
+    histogram_to_fit = TH1D(hname, htitle, nbins_to_fit, array('d',list_limits))
+
+    # Fill histogram suitable to fit
+    for b in range(1, nbins + 1):
+        value = histogram.GetBinContent(b)
+        error = histogram.GetBinError(b)
+
+        # Redefine bin number when folding
+        if (fit_method is "Fd"):
+            is_first_half = (nbins_to_fit - b + 1 > 0)
+            # Get correct bin position of the first half in the new histogram
+            if is_first_half:
+                b = nbins_to_fit - b + 1
+            # Same with second half
+            else:
+                b = b - bin_zero + 1
+
+            # Obtain value in new histogram (zero if empty)
+            value_new = histogram_to_fit.GetBinContent(b)
+            # Update value if non-zero (i.e. we are in second half)
+            if value_new:
+                value = value + value_new
+                error_new = histogram_to_fit.GetBinError(b)
+                error = TMath.Sqrt(error*error + error_new*error_new)
+
+        histogram_to_fit.SetBinContent(b, value)
+        histogram_to_fit.SetBinError(b, error)
+
+    # Define corrected plot style
+    histogram_to_fit.SetMinimum(0.0001)
+    histogram_to_fit.SetMaximum(1.4 * histogram_to_fit.GetMaximum())
+    histogram_to_fit.GetYaxis().SetMaxDigits(3)
+
+    return histogram_to_fit
+
+def get_fit_function(histogram_to_fit, tf1_name, fit_method, cut_str):
+# Return TF1 with the correct fit range according to the method and cuts
+    skip_central_peak = ms.cut_is_included("NP", cut_str)
+    add_sin_term = ms.cut_is_included("Fs", cut_str)
+
+    # Create function string
+    fit_str = "[0] + [1]*cos(TMath::Pi()*x/180.0) +"\
+                   " [2]*cos(2*TMath::Pi()*x/180.0)"
+    if add_sin_term:
+        fit_str+= "+ [3]*sin(TMath::Pi()*x/180.0)"
+
+    # Select correct fit range
+    nbins = histogram_to_fit.GetXaxis().GetNbins()
+    bin_i, bin_f = 1, nbins
+    xmin = histogram_to_fit.GetBinLowEdge(bin_i)
+    xmax = histogram_to_fit.GetBinLowEdge(bin_f + 1)
+    # Change values for LR method
+    bin_zero = histogram_to_fit.FindBin(0.0)
+    if fit_method is "LR":
+        if "L" in tf1_name:
+            xmax = histogram_to_fit.GetBinLowEdge(bin_zero + 1)
+        else:
+            xmin = histogram_to_fit.GetBinLowEdge(bin_zero)
+
+    # Skip bin zero if required
+    if skip_central_peak:
+        # fit_function.RejectPoint(bin_zero) # Did not work :(
+        if (fit_method is "Fd") or (fit_method is "Sh"):
+            xmin = histogram_to_fit.GetBinLowEdge(bin_i + 1)
+        elif (fit_method is "LR"):
+            if "L" in tf1_name:
+                xmax = histogram_to_fit.GetBinLowEdge(bin_zero)
+            else:
+                xmin = histogram_to_fit.GetBinLowEdge(bin_zero + 1)
+
+    # Create fit object TF1
+    fit_function = TF1(tf1_name, fit_str, xmin, xmax)
+
+    return fit_function
 
 # Construct the argument parser
 parser = optparse.OptionParser("usage: %prog [options]\n")
@@ -220,6 +144,8 @@ parser.add_option('-D', dest='Dataset', default = "",
                   help="Dataset in format <targ>_<binType>_<Ndims>")
 parser.add_option('-J', dest='isJLab', action='store_true', default = False,
                   help="Use folder from JLab_cluster")
+parser.add_option('-b', dest='nonIntegratedVars', default = "",
+                  help="Add non-integratedd bins like in QNZ")
 parser.add_option('-i', dest='inputCuts', default = "",
                   help="Add input cuts Xf_Yb_...")
 parser.add_option('-o', dest='outputCuts', default = "",
@@ -235,12 +161,15 @@ dataset = options.Dataset
 isJLab = options.isJLab
 ovr = options.Overwrite
 
+bin_set = options.nonIntegratedVars
 input_cuts = options.inputCuts
 plots_cuts = options.inputCuts +"_"+ options.outputCuts
+if bin_set:
+    input_cuts+= "_b%s"%bin_set
+    plots_cuts+= "_b%s"%bin_set
 
-d_bin = ms.get_name_dict(dataset)
-m_fit = ms.get_fit_method(plots_cuts)
-
+fit_method = ms.get_fit_method(plots_cuts)
+# d_bin = ms.get_name_dict(dataset)
 
 in_obj = nf.naming_format("Correction", dataset, cuts=input_cuts,
                           is_JLab=isJLab)
@@ -248,93 +177,87 @@ inputfile = TFile(in_obj.get_path(),"READ")
 
 out_obj = nf.naming_format("Fit", dataset, cuts=plots_cuts,
                           is_JLab=isJLab)
-# outputfile = TFile(out_obj.get_path(True),"RECREATE")
 
-l_hists = inputfile.GetListOfKeys()
-l_ffit_names = out_obj.get_l_fitnames()
+# Get dataset info
+dataset_info = ms.get_name_dict(dataset)
+nbin_dataset = dataset_info["nBin"]
+target = dataset_info["Target"]
 
-# Create and save output
+# Retrieve all saved in input file
+list_of_keys = inputfile.GetListOfKeys()
+
+# Create canvas and output
 canvas = ms.create_canvas()
 outputfile = TFile(out_obj.get_path(True, ovr),"RECREATE")
-phi_axis_title = ms.axis_label('I',"LatexUnit") # "#phi_{PQ} (deg)"
 
-gStyle.SetTitleYOffset(1.2)
-
-for h in l_hists:
-    obj_type = h.ReadObj().Class_Name()
-
-    if "TH1D" not in obj_type:
+for key in list_of_keys:
+    key_type = key.ReadObj().Class_Name()
+    # Skip if object is not an histogram
+    if "TH1" not in key_type:
         continue
-    hname = h.GetName()
+
+    # Use corrected histograms only
+    hname = key.GetName()
     if "raw" in hname.lower():
         continue
 
-    # Name format: h(name)_(acc_meth)_(bincode)
-    _, cor_meth, bincode = hname.split("_")
+    # Check if fit can be done
+    histogram = key.ReadObj()
+    has_problems = histogram_has_problems(histogram, fit_method)
+    if has_problems:
+        continue
 
+    # Update output object (Name format: h(name)_(acc_meth)_(bincode))
+    _, cor_meth, bincode = hname.split("_")
     out_obj.updt_acc_method(cor_meth)
     out_obj.updt_bin_code(bincode)
-
-    # Get Histogram
-    hist = h.ReadObj()
-
-    # Check feasibility of applying fit
-    fit_work = check_fit_feasibility(hist, m_fit)
-    if not fit_work:
-        continue
+    hname_new = out_obj.get_hist_name()
+    list_fit_names = out_obj.get_l_fitnames()
 
     # Normalize distribution before perform fit
     if ms.cut_is_included("Nm", plots_cuts):
-        hist.Scale(1.0 / hist.Integral("width"))
+        histogram.Scale(1.0 / histogram.Integral("width"))
 
-    # Get corrected histogram with proper axes depending on method
-    hist_corr = get_corrected_hist(hist, out_obj.get_hist_name(), m_fit)
+    # Get histogram to fit according to method selected
+    histogram_to_fit = get_correct_histogram_to_fit(histogram, hname_new, fit_method)
+    histogram_to_fit.Draw("hist axis")
 
-    hist_corr.SetMinimum(0.0001)
-    hist_corr.SetMaximum(1.4 * hist_corr.GetMaximum())
-    hist_corr.Draw("hist axis")
+    # Make fit for each fit name available
+    for i,fname in enumerate(list_fit_names):
+        fit = get_fit_function(histogram_to_fit, fname, fit_method, plots_cuts)
+        fit_color = colors[5] if i is 0 else colors[1]
+        fit.SetLineColor(fit_color)
+        fit_opts = "MSRQ" if "L" not in fname else "MSRQ+"
+        fit_matrix = histogram_to_fit.Fit(fname, fit_opts, "")
 
-    # Return list of TF1 with the proper range
-    # Use plots_cuts to find special cuts/options
-    l_tf1 = get_fit_functions(hist_corr, out_obj.get_l_fitnames(),
-                              m_fit, plots_cuts)
+        # Save covariance and correlation matrices
+        covariance_name = out_obj.get_matrix_name("cov", i)
+        fit_matrix.GetCovarianceMatrix().Write(covariance_name)
+        correlation_name = out_obj.get_matrix_name("corr", i)
+        fit_matrix.GetCorrelationMatrix().Write(correlation_name)
 
-    # Make fit and save covariance and correlation matrices
-    l_chi2 = []
-    for i,fitfunc in enumerate(l_tf1):
-        this_fopts = "MSRQ"
-        if (i==1):
-            this_fopts+= "+"
+        # Draw fit info
+        chi_square = fit.GetChisquare()
+        ndf = fit.GetNDF()
+        fit_info_str = "#chi^{2} / ndf = %.2f / %i"%(chi_square, ndf)
+        if ndf:
+            fit_info_str+= " = %.2f"%(chi_square/ndf)
 
-        fit_matrix = hist_corr.Fit(l_ffit_names[i], this_fopts, "")
-        name_cov = out_obj.get_matrix_name("cov", i)
-        fit_matrix.GetCovarianceMatrix().Write(name_cov)
-
-        name_corr = out_obj.get_matrix_name("corr", i)
-        fit_matrix.GetCorrelationMatrix().Write(name_corr)
-
-        ## Draw chi2 info
-        d_pos_tex = {
-            "LR": [15.0, -15.0], "Fd": [90.0], "Sh": [180.0], "Ff": [0.0],
-        }
-
-        xpos_tex = d_pos_tex[m_fit][i] if m_fit in d_pos_tex else 0.0
-        ypos_tex = 1.1 * hist.GetMaximum()
-
-        info_chi2 = get_chi2ndf(fitfunc, xpos_tex,ypos_tex, "%s%i"%(m_fit,i))
-        l_chi2.append(info_chi2)
-
-    for c in l_chi2:
-        c.Draw()
+        fit_info_Latex = TLatex()
+        x_fit = ms.get_pad_center()
+        y_fit = 1 - 2*margin - 0.02 - i*margin
+        fit_info_Latex.SetTextAlign(23)
+        fit_info_Latex.SetTextColor(fit_color)
+        fit_info_Latex.SetTextSize(ms.get_size() - 6)
+        fit_info_Latex.DrawLatexNDC(x_fit, y_fit, fit_info_str)
 
     # Draw and save
-    hist_corr.GetYaxis().SetMaxDigits(3)
-    hist_corr.Draw("FUNC same")
-    hist_corr.Write()
+    histogram_to_fit.Draw("FUNC same")
+    histogram_to_fit.Write()
 
     ms.draw_preliminary("Correction fit")
     ms.draw_targetinfo(ms.get_name_format(dataset), "Data")
-    ms.draw_bininfo(bincode, d_bin["nBin"])
+    ms.draw_bininfo(bincode, nbin_dataset)
 
     out_obj.updt_extension("png")
     canvas.SaveAs(out_obj.get_path())
